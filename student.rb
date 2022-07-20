ca63b9854ed7fbca6ffcb679a06c92be94953edf
Jake Felice <jasonfelice0@gmail.com>
2022-07-20 01:08:19 -0700

    Pass parent_permissions in super

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require './person.rb'
 
 class Student < Person
     def initialize(classroom, age, name = "Unknown", parent_permission: true)
-        super(age, name)
+        super(age, name, parent_permission)
         @classroom = classroom
     end
 
