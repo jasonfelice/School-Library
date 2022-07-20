9704df95f0f23887f7bd8bce0ec7af93279a9931
Jake Felice <jasonfelice0@gmail.com>
2022-07-20 01:17:19 -0700

    Remove parent_permissions from child classes

--- student.rb
+++ student.rb
@@ -1,8 +1,8 @@
 require './person'
 
 class Student < Person
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(classroom, age, name = 'Unknown')
+    super(age, name)
     @classroom = classroom
   end
 
