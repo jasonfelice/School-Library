378e3cfe4133b5db889c002fff476a243a0c97c4
Jake Felice <jasonfelice0@gmail.com>
2022-07-19 23:36:20 -0700

    Create the methods for person class

--- person.rb
+++ person.rb
@@ -1,8 +1,20 @@
 class Person
-    def initialize(name = "Unkown", age, parent_permission = true)
+    attr_accessor :name, :age
+    attr_reader :id
+    def initialize(age, name = "Unkown", parent_permission = true)
         @id = Random.rand(1..1000)
         @name = name
         @age = age
         @parent_permission = parent_permission
+    end
+
+    def can_use_services?
+        return @parent_permission
+    end
+
+    private
+
+    def is_of_age?
+        return @age >= 18
     end
 end
