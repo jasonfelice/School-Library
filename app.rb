75e76ea1267ff0c602465882d75ad78cf771e745
Jake Felice <jasonfelice0@gmail.com>
2022-07-22 10:55:28 -0700

    Create list_books method

--- app.rb
+++ app.rb
@@ -23,26 +23,30 @@ class App
   end
 
   def list_people
-    puts "List People Working!"
+    puts 'List People Working!'
   end
 
   def list_books
-    puts "List Books Working!"
+    puts '***Books***'
+    puts "Opps! You don't have any books. Try creating one (option 4)." if @books.empty?
+    @books.each do |book|
+      puts "#{book.title} by #{book.author}"
+    end
   end
 
   def create_person
-    puts "Create person Working!"
+    puts 'Create person Working!'
   end
 
   def create_book
-    puts "Create book Working!"
+    puts 'Create book Working!'
   end
 
   def create_rental
-    puts "Create rental Working!"
+    puts 'Create rental Working!'
   end
 
   def list_rentals
-    puts "List rental Working!"
+    puts 'List rental Working!'
   end
 end
