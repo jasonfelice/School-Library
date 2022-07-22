83316170ea4deb24c10523e0ba61814b5008f7fd
Jake Felice <jasonfelice0@gmail.com>
2022-07-22 09:39:05 -0700

    Create options list

--- app.rb
+++ app.rb
@@ -9,4 +9,16 @@ class App
     @books = []
     @people = []
   end
+
+  def run
+    puts 'Welcome to the School Library!'
+    until list_options
+      input = gets.chomp
+      if input == '7'
+        puts 'Have good day :)'
+        break
+      end
+      option(input)
+    end
+  end
 end
--- main.rb
+++ main.rb
@@ -4,4 +4,34 @@ def main
   App.new.run
 end
 
+def list_options
+  puts 'Select one of the following options: '
+  puts '1 -> List all books'
+  puts '2 -> List all people'
+  puts '3 -> Create a person'
+  puts '4 -> Create a book'
+  puts '5 -> Create a rental'
+  puts '6 -> List all rentals for a given person id'
+  puts '7 -> Exit'
+end
+
+def option(input)
+  case input
+  when '1'
+    list_books
+  when '2'
+    list_people
+  when '3'
+    create_person
+  when '4'
+    create_book
+  when '5'
+    create_rental
+  when '6'
+    list_rentals
+  else
+    puts "I don't understand..."
+  end
+end
+
 main
