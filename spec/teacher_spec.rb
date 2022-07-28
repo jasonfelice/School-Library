bb1161cf19831a0549d1fc8c23484ba7a3bd39b0
Jake Felice <jasonfelice0@gmail.com>
2022-07-28 00:40:43 -0700

    Write test for teacher:

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../teacher'
+describe Teacher do
+  it 'is an instance of Teacher Class' do
+    teacher = Teacher.new('Engineer', 65, 'Chidinma')
+    expect(teacher).to be_instance_of(Teacher)
+  end
+  it 'a teacher can use service regardless of age' do
+    teacher = Teacher.new('Engineer', 65, 'Chidinma')
+    expect(teacher.can_use_services?).to eq(true)
+  end
+end
